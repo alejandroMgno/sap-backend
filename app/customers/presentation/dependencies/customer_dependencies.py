1d1d79e2ef8beb25f31945e1da40e3486aca46ec
Jose Alejandro Rubio <alejandro.rub.men@gmail.com>
2026-07-16 17:18:32 -0600

    feat(customers): register create customer dependency

--- app/customers/presentation/dependencies/customer_dependencies.py
+++ app/customers/presentation/dependencies/customer_dependencies.py
@@ -1,6 +1,16 @@
 from app.customers.infrastructure.container.container import Container
+from app.customers.application.use_cases.create_customer import (
+    CreateCustomerUseCase,
+)
+from app.customers.infrastructure.repositories.memory_customer_repository import MemoryCustomerRepository
 
 
 def get_list_customers_use_case():
 
     return Container.list_customers_use_case()
+
+def get_create_customer_use_case():
+
+    repository = MemoryCustomerRepository()
+
+    return CreateCustomerUseCase(repository)
