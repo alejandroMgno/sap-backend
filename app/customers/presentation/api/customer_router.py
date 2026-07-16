737ea29d9677b8e8d5deb4335c53be91c2fce140
Jose Alejandro Rubio <alejandro.rub.men@gmail.com>
2026-07-16 17:18:52 -0600

    feat(customers): expose create customer endpoint

--- app/customers/presentation/api/customer_router.py
+++ app/customers/presentation/api/customer_router.py
@@ -5,6 +5,17 @@ from app.customers.presentation.schemas.customer_schema import CustomerResponse
 from app.customers.presentation.dependencies.customer_dependencies import (
     get_list_customers_use_case,
 )
+from app.customers.presentation.schemas.customer_schema import (
+    CustomerCreate,
+    CustomerResponse,
+)
+from app.customers.application.use_cases.create_customer import (
+    CreateCustomerUseCase,
+)
+
+from app.customers.presentation.dependencies.customer_dependencies import (
+    get_create_customer_use_case,
+)
 
 
 router = APIRouter(
@@ -26,3 +37,20 @@ def list_customers(
     customers = use_case.execute()
 
     return customers
+
+@router.post(
+    "",
+    response_model=CustomerResponse,
+    status_code=201,
+)
+def create_customer(
+    customer: CustomerCreate,
+    use_case: CreateCustomerUseCase = Depends(
+        get_create_customer_use_case
+    ),
+):
+
+    return use_case.execute(
+        name=customer.name,
+        email=customer.email,
+    )
